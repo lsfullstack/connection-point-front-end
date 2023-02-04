63a15b9bee8c45b39e3ea37e4407af1b16902e10
Lucas <lsengenheirocivil@gmail.com>
2023-02-04 18:38:47 -0300

    feat: create form component

---
+++ src/components/Form/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+`;
+
+export default Container;
