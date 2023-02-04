4ba7a8be44eaef85bf6edad0cdfe9639463f6f60
Lucas <lsengenheirocivil@gmail.com>
2023-02-04 20:46:36 -0300

    feat: create register page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 
 const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage/>} />
+      <Route path="/cadastro" element={<RegisterPage/>} />
     </Routes>
   );
 }
